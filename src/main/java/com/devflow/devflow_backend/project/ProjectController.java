e7917484e7c8c5026cc99c2b39b719c5e59f97c3
abdullahmaverick09 <abdullah.maverick09@gmail.com>
2026-02-17 11:15:04 +0500

    Feat: Add update endpoint for the Projects - Implemented UpdateProject logic in Service - Added PUT endpoint in Controller - Tested successfully via curl and DB verification

--- src/main/java/com/devflow/devflow_backend/project/ProjectController.java
+++ src/main/java/com/devflow/devflow_backend/project/ProjectController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,11 @@ public class ProjectController {
     @GetMapping
     public ResponseEntity<List<Project>> getAllProjects(){
         return ResponseEntity.ok(projectService.getAllProjects());
+    }
+    
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Project> updateProject(@PathVariable Long id , @RequestBody Project projectDetails){
+    		return ResponseEntity.ok(projectService.updateProject(id, projectDetails));
     }
 }
--- src/main/java/com/devflow/devflow_backend/project/ProjectService.java
+++ src/main/java/com/devflow/devflow_backend/project/ProjectService.java
@@ -1,6 +1,7 @@
 package com.devflow.devflow_backend.project;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -9,16 +10,23 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
-    private final ProjectRepository projectRepository;
+	private final ProjectRepository projectRepository;
 
-    
-    public Project createProject(Project project){
-        return projectRepository.save(project);
-    }
+	public Project createProject(Project project) {
+		return projectRepository.save(project);
+	}
 
+	public List<Project> getAllProjects() {
+		return projectRepository.findAll();
+	}
 
-    public List<Project> getAllProjects(){
-        return projectRepository.findAll();
-    }
-    
+	public Project updateProject(Long id, Project projectDetails) {
+		Project existingProject = projectRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Project with the id: " + id + " is not found"));
+		existingProject.setName(projectDetails.getName());
+		existingProject.setDescription(projectDetails.getDescription());
+		
+		return projectRepository.save(existingProject);
+}
+
 }
